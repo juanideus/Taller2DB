4d44b694d09655f3386bbf36fe917703588b13b4
juanideus <juan.zuniga@alumnos.ucn.cl>
2026-05-20 21:58:05 -0400

    fix: se arregla .env

--- Src/Db/Db.js
+++ Src/Db/Db.js
@@ -1,12 +1,12 @@
 import mysql from "mysql2/promise";
 import dotenv from "dotenv";
+const result = dotenv.config();
 
-dotenv.config();
 const pool = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 const getConnection = async () => {
@@ -20,12 +20,11 @@ const getConnection = async () => {
   }
 };
 export const executeQuery = async (query, params) => {
-    try{
-        const rows = await pool.query(query, params);
-        return rows[0];
-    
-    }catch(error){
-        console.error("Error al ejecutar la consulta:",query, error);
-        throw error;
-    }
-}
+  try {
+    const rows = await pool.query(query, params);
+    return rows[0];
+  } catch (error) {
+    console.error("Error al ejecutar la consulta:", query, error);
+    throw error;
+  }
+};
